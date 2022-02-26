85dc5bacc0b024bbdcfd1bdb354f4a1adb76c6c3
Erin M <erin.michon@gmail.com>
2022-02-26 15:56:23 -0500

    project ex rendering hardcoded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import About from './components/About';
+import Project from './components/Project';
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
       <main>
+        <Project></Project>
         <About></About>
       </main>
       <Footer></Footer>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import passGenApp from "../../assets/images/passGenApp.gif"
 
-function Project() {
+
+function Project(props) {
+  const currentProject = {
+    title: "password generator",
+    deployed: "https://erin-michon.github.io/password-generator/",
+    github: "https://github.com/erin-michon/password-generator"
+  }
   return (
     <section>
-        <h1> This is where the projects will render</h1>
+      <div className="flex-row">
+        <img src={passGenApp} alt="a gif of the password generator accepting criteria from user and providing a generated password"></img>
+      </div>
+      <h1>{currentProject.title}</h1>
+      <a href={currentProject.deployed}>Deployed Application</a>
+      <a href={currentProject.github}>GitHub Repository</a>
     </section>
   );
 }

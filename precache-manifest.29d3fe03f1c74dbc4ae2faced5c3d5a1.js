8335228dc23c0727ab85da30e7ae2c5b16113d4b
Erin M <erin.michon@gmail.com>
2022-06-11 22:20:59 -0400

    Updates

---
+++ precache-manifest.29d3fe03f1c74dbc4ae2faced5c3d5a1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f1ce83b28920a1e0772",
+    "url": "/emichon-react-port/static/css/main.1fc60b78.chunk.css"
+  },
+  {
+    "revision": "9f1ce83b28920a1e0772",
+    "url": "/emichon-react-port/static/js/main.9f1ce83b.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/emichon-react-port/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "8b5db89864f104e01805",
+    "url": "/emichon-react-port/static/css/2.6f97f295.chunk.css"
+  },
+  {
+    "revision": "8b5db89864f104e01805",
+    "url": "/emichon-react-port/static/js/2.8b5db898.chunk.js"
+  },
+  {
+    "revision": "768c9b80ef1277b90662",
+    "url": "/emichon-react-port/static/js/runtime~main.768c9b80.js"
+  },
+  {
+    "revision": "48db5a3166157a9305a164ee9bbcce07",
+    "url": "/emichon-react-port/static/media/emichonpic.48db5a31.jpg"
+  },
+  {
+    "revision": "32830d5e4e23dcb0185c183b8c7ba819",
+    "url": "/emichon-react-port/static/media/1.32830d5e.gif"
+  },
+  {
+    "revision": "2515879ad2aaace42fe1d1d9b78a0854",
+    "url": "/emichon-react-port/static/media/2.2515879a.gif"
+  },
+  {
+    "revision": "2c5d87114c202c8278ee0113dd98f345",
+    "url": "/emichon-react-port/static/media/3.2c5d8711.gif"
+  },
+  {
+    "revision": "2c1b259c519bd2c8b0de0bfc9f6c3dcb",
+    "url": "/emichon-react-port/static/media/4.2c1b259c.gif"
+  },
+  {
+    "revision": "60fb0b3d8f5d4c8beac36aadefccadb7",
+    "url": "/emichon-react-port/static/media/5.60fb0b3d.gif"
+  },
+  {
+    "revision": "5dc7943abbf5ff29bbe60eb5342b96c6",
+    "url": "/emichon-react-port/static/media/6.5dc7943a.gif"
+  },
+  {
+    "revision": "23ae113126de4d3e9cdd161ea91e0ef5",
+    "url": "/emichon-react-port/index.html"
+  }
+];
